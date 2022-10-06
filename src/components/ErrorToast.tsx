a81805740d456489dea5426043a2324b6fb105cb
M@HeR <benbaccar.maher@gmail.com>
2022-10-06 15:13:13 +0100

    refactor : refactor `error toast` function

--- src/components/ErrorToast.tsx
+++ src/components/ErrorToast.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
 import { toast } from 'react-toastify';
 
-export default function ErrorToast(message: string): JSX.Element {
-  return (
-    <React.Fragment>
-      {toast.error(message, {
-        className: 'error-toast',
-        position: toast.POSITION.TOP_CENTER,
-        autoClose: 10000,
-        theme: 'colored',
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-      })}
-    </React.Fragment>
-  );
+export default function ErrorToast(message: string): void {
+  toast.error(message, {
+    className: 'error-toast',
+    position: toast.POSITION.TOP_CENTER,
+    autoClose: 10000,
+    theme: 'colored',
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
 }
